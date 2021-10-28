7aab90733bb3281c1affbe91fe3f152109d4622f
Lagutov Alexander <LVGUTXV@ya.ru>
2021-10-29 02:58:58 +0300

    Added def "is_hex"

--- lib_crypto/utils/def_str.py
+++ lib_crypto/utils/def_str.py
@@ -33,6 +33,15 @@ def replace_special(text, alph=alph, replaces=REPLACES):
     return text
 
 
+def is_hex(s):
+    import string
+
+    try:
+        return all(c in string.hexdigits for c in s)
+    except:
+        return False
+
+
 if __name__ == "__main__":
     test = "Это тестовая строка, тут есть все: запятые, КАПС, тире(-), и многое другое... That`s awesome!"
     print(clear_text(test))
