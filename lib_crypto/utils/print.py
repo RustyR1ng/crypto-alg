e41620b1d76517576a5f247c6e4fa8e50b9c5a6d
Lagutov Alexander <LVGUTXV@ya.ru>
2021-10-29 20:06:12 +0300

    Refactor

--- lib_crypto/utils/print.py
+++ lib_crypto/utils/print.py
@@ -2,8 +2,16 @@ from rich import print
 
 
 def print_header(text):
-    print(f"[bold black on bright_white] { text } [/bold black on bright_white]")
+    print(header(text))
 
 
 def print_kv(k, v):
-    print(f"[bold cyan] { k } :[/bold cyan] { v } ")
+    print(kv(k, v))
+
+
+def header(text):
+    return f"[bold black on bright_white] { text } [/bold black on bright_white]"
+
+
+def kv(k, v):
+    return f"[bold cyan] { k } :[/bold cyan] { v } "
