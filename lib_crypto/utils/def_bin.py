729048e0c87b6cfca5383e408999423e308c4fd4
Lagutov Alexander <LVGUTXV@ya.ru>
2021-10-29 03:04:01 +0300

    "def_bin" contains functions for working with binary

---
+++ lib_crypto/utils/def_bin.py
@@ -0,0 +1,40 @@
+from .def_str import is_hex
+from pygost.utils import hexdec, hexenc
+
+
+def to_binary(plain):
+    """Converts plaintext to binary"""
+
+    binary = [
+        int(i)
+        for i in "".join(block[2:] for block in map(bin, bytearray(plain, "utf-8")))
+    ]
+
+    return binary
+
+
+def bin_to_str(bits, encoding="UTF-8", errors="surrogatepass"):
+    """Converts binary to string."""
+
+    n = int(bits, 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, "big").decode(encoding, errors) or "\0"
+
+
+def str_to_bytes(text: str) -> bytes:
+    b_text = None
+    if is_hex(text):
+        b_text = hexdec(text)
+    else:
+        b_text = bytes(text, "utf-8")
+
+    return b_text
+
+
+def bytes_to_str(b_text: bytes, _return: str = "str") -> str:
+    text = ""
+    if _return == "str":
+        text = b_text.decode("utf-8")
+    else:
+
+        text = hexenc(b_text)
+    return text
