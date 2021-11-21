a8258ce9df5dd2a79d1edc3f59fea56ca9f1d6a3
Lagutov Alexander <LVGUTXV@ya.ru>
2021-11-21 16:45:03 +0300

    b11: Added Diffie Hellman alg

---
+++ lib_crypto/b11/Diffie_Hellman.py
@@ -0,0 +1,47 @@
+"""
+В протоколе обмена секретными ключами предполагается, что все
+пользователи знают некоторые числа n и a (1< a < n). Для выработки общего
+секретного ключа пользователи A и B должны проделать следующую процедуру:
+"""
+
+"""
+1. Определить секретные ключи пользователей КА и КВ.
+2. Для этого каждый пользователь независимо выбирает случайные числа из
+интервала (2,..., n-1).
+3. Вычислить открытые ключи пользователей YA и YB:
+ Y=a^K mod n
+4. Обменяться ключами YA и YB по открытому каналу связи.
+5. Независимо определить общий секретный ключ К:
+ KA=Y^KA mod n
+ KB=Y^KB mod n.
+
+KA = KB = K
+"""
+
+from ..utils.print import print_kv
+
+
+def gen(n, a, priv_key_A, priv_key_B):
+    assert 1 < a < n
+    assert all(2 < i < (n - 1) for i in (priv_key_A, priv_key_B))  # private keys
+
+    pub_A, pub_B = (
+        (a ** key) % n for key in (priv_key_A, priv_key_B)
+    )  # public keys (yA, yB)
+
+    key_A, key_B = (
+        ((public ** private) % n)
+        for public, private in zip((pub_A, pub_B), (priv_key_B, priv_key_A))
+    )  # joint private key
+
+    result_key = key_A if key_A == key_B and key_A > 1 else None
+
+    return result_key
+
+
+if __name__ == "__main__":
+    n, a, key_A, key_B = 9, 4, 4, 5
+    print_kv("n, a, key_A, key_B", (n, a, key_A, key_B))
+
+    res = gen(n, a, key_A, key_B)
+    print_kv("Ключ", res)
