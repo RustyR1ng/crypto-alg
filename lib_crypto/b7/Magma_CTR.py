42d6b236c1d2e3a4df1d103869c45d1cc3a2017e
RustyR1ng <LVGUTXV@ya.ru>
2021-12-01 15:27:19 +0300

    Add Magma mode CTR

---
+++ lib_crypto/b7/Magma_CTR.py
@@ -0,0 +1,58 @@
+from pygost.gost3412 import GOST3412Magma as Magma
+from pygost.utils import hexdec, hexenc
+from pygost.gost3413 import ctr
+from ..utils.def_str import is_hex
+
+
+def ctr_encrypt(text: str, key: str, iv: str) -> str:
+    key, iv = hexdec(key), hexdec(iv)
+    crypter = Magma(key)
+
+    if is_hex(text):
+        text = hexdec(text)
+    else:
+        text = text.encode()
+    res = ctr(crypter.encrypt, crypter.blocksize, text, iv)
+
+    return hexenc(res)
+
+
+def ctr_decrypt(text: str, key: str, iv: str, _return="hex") -> str:
+    key, iv, text = hexdec(key), hexdec(iv), hexdec(text)
+    crypter = Magma(key)
+
+    res = ctr(crypter.encrypt, crypter.blocksize, text, iv)
+
+    return hexenc(res) if _return == "hex" else res.decode()
+
+
+def main():
+    from ..utils.print import print_header, print_kv
+    from ..utils.data import text_1000
+
+    key = "ffeeddccbbaa99887766554433221100f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff"
+    IV = "12345678"
+
+    plaintext = "92def06b3c130a59"
+    plaintext += "db54c704f8189d20"
+    plaintext += "4a98fb2e67a8024c"
+    plaintext += "8912409b17b57e41"
+
+    print_header("Тест ГОСТ гаммирование")
+
+    enc = ctr_encrypt(plaintext, key, IV)
+    dec = ctr_decrypt(enc, key, IV)
+
+    print_kv("Шифр", enc)
+    print_kv("Расшифровка", dec)
+
+    print_header("Тест на 1000")
+    enc = ctr_encrypt(text_1000, key, IV)
+    dec = ctr_decrypt(enc, key, IV, "text")
+
+    print_kv("Шифр 1000", enc)
+    print_kv("Расшифровка", dec)
+
+
+if __name__ == "__main__":
+    main()
