e1e8512c65304c02dc4ba5b9aba45efdbb4f2bd9
RustyR1ng <LVGUTXV@ya.ru>
2021-12-29 20:25:08 +0300

    _

--- lib_crypto/b8/RSA.py
+++ lib_crypto/b8/RSA.py
@@ -15,7 +15,7 @@ def enc(text: str, p: str, q: str, e: str):
     euler = (p - 1) * (q - 1)  # Функция Эйлера
     d = inverse_of(e, euler)
     m = to_indexes(clear_text(text))
-    enc_m = [enc_1(symbol, n, e) for symbol in m]
+    enc_m = " ".join([enc_1(symbol, n, e) for symbol in m])
 
     return f"{enc_m} d={d}, n={n}"
 
@@ -26,6 +26,7 @@ def dec_1(num: int, d: int, n: int):
 
 # Расширвание
 def dec(text: str, d: str, n: str):
+    text = text.split()
     d, n = map(int, (d, n))
     m = [dec_1(int(symbol), d, n) for symbol in text]
     m = to_symbols(m)
